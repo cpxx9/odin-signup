2ab7bd96e5ca994f047e09b65828ed09195bb578
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-01-15 08:28:56 -0500

    started laying out todos in JS file

---
+++ JS/scripts.js
@@ -0,0 +1,7 @@
+//create div element if width of device is more than 900px wide
+//this will be a short description of the service
+
+//check that value entered to phone number is a 9 digit int
+
+//verify password and confirm password match
+//let the user know before they submit
