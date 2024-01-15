60cd0a96f5f4dcde2941f156e1aa9a92e4cfce20
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-01-15 15:17:33 -0500

    added password verification and phone formatting

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,7 +1,41 @@
-//create div element if width of device is more than 900px wide
-//this will be a short description of the service
+//phone input add hyphens only allow digits
+var phoneNumber = document.querySelector('#phone_number');
 
-//check that value entered to phone number is a 9 digit int
+phoneNumber.addEventListener('keyup', function(e){
+  if (e.key != 'Backspace' && phoneNumber.value.length === 3) {
+    phoneNumber.value = '(' + phoneNumber.value + ')';
+  } else if (e.key != 'Backspace' && phoneNumber.value.length === 8) {
+    phoneNumber.value += '-';
+  }
+});
 
 //verify password and confirm password match
 //let the user know before they submit
+document.getElementById('password_confirm').addEventListener("keyup", validatePassword);
+
+function validatePassword() {
+  let password = document.getElementById('password').value;
+  let confirmPassword = document.getElementById('password_confirm').value;
+  console.log(confirmPassword);
+
+  if (password != confirmPassword) {
+    document.getElementById('wrong_pass_alert').style.color = 'red';
+    document.getElementById('wrong_pass_alert').innerHTML = '☒ Use same password';
+    document.getElementById('create').disabled = true;
+    document.getElementById('create').style.opacity = (0.8);
+  } else {
+    document.getElementById('wrong_pass_alert').style.color = 'green';
+    document.getElementById('wrong_pass_alert').innerHTML = '🗹 Passwors match';
+    document.getElementById('create').disabled = false;
+    document.getElementById('create').style.opacity = (1);
+}
+}
+
+function wrong_pass_alert() {
+  if (document.getElementById('password').value != "" &&
+      document.getElementById('password_confirm').value != "") {
+      alert("Your response is submitted");
+  } else {
+      alert("Please fill all the fields");
+  }
+}
